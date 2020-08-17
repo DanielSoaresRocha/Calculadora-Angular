d1cd878e401da044dcbdbc3e48df4a0ddaf6e73e
DanielSoaresRocha <legoliga123@gmail.com>
2020-08-17 16:07:56 -0300

    criando módulo calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CalculadoraModule } from './calculadora'
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalculadoraModule
   ],
   providers: [],
   bootstrap: [AppComponent]
